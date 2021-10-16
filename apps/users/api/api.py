d855f3dde80c4d7f5a253e662191437195cc6424
Israel Jasma <by.darklancer@gmail.com>
2021-10-16 17:02:27 -0300

    Feat: CRUD User

--- apps/users/api/api.py
+++ apps/users/api/api.py
@@ -1,12 +1,41 @@
-from rest_framework.response import Response
+import re
 from rest_framework.response import Response
 from rest_framework.views import APIView
+from rest_framework.decorators import api_view
 from apps.users.models import User
 from apps.users.api.serializers import UserSerializer
 
-class UserAPIView(APIView):
+@api_view(['GET', 'POST'])
+def user_api_view(request):
 
-    def get(self, request):
+    if request.method == 'GET':
         users = User.objects.all()
         users_serializer = UserSerializer(users, many = True)
         return Response(users_serializer.data)
+
+    elif request.method == 'POST':
+        users_serializer = UserSerializer(data = request.data)
+        if users_serializer.is_valid():
+            users_serializer.save()
+            return Response(users_serializer.data)
+        return Response(users_serializer.errors)
+
+@api_view(['GET', 'PUT', 'DELETE'])
+def user_detail_api_view(request, pk = None):
+    if request.method == 'GET':
+        user = User.objects.filter(id = pk).first()
+        user_serializer = UserSerializer(user)
+        return Response(user_serializer.data)
+    
+    elif request.method == 'PUT':
+        user = User.objects.filter(id = pk).first()
+        user_serializer = UserSerializer(user, data = request.data)
+        if user_serializer.is_valid():
+            user_serializer.save()
+            return Response(user_serializer.data)
+        return Response(user_serializer.errors)
+    
+    elif request.method == 'DELETE':
+        user = User.objects.filter(id = pk).first()
+        user.delete()
+        return Response('Eliminado')
