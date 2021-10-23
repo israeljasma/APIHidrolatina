d76391727910f2dbe289f2b0894529a620b001eb
Israel Jasma <by.darklancer@gmail.com>
2021-10-23 15:13:34 -0300

    Feat: Serializar Relaciones en Modelos

--- apps/identificationNFC/api/serializers.py
+++ apps/identificationNFC/api/serializers.py
@@ -10,7 +10,17 @@ class ActiveModelSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class IdentificationSerializer(serializers.ModelSerializer):
-
+    # active = serializers.StringRelatedField()
+    active = ActiveModelSerializer()
     class Meta:
         model = identification
         fields = '__all__'
+
+    # def to_representation(self, instance):
+    #     return {
+    #         'id': instance.id,
+    #         'active' : instance.active,
+    #         # 'description' : instance.description,
+    #         # 'created_date' : instance.created_date,
+    #         # 'modified_date' : instance.modified_date
+    #     }
