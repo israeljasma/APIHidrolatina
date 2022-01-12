aa6fb15f471ea7925d42c599fa5f0ccaf0e0b643
Israel Jasma <by.darklancer@gmail.com>
2022-01-12 16:38:57 -0300

    Feat: state agregado a idenificacion

--- apps/identificationNFC/models.py
+++ apps/identificationNFC/models.py
@@ -33,6 +33,7 @@ class activeModel(models.Model):
 class identification(models.Model):
     id = models.AutoField(primary_key = True)
     NFC = models.CharField('NFC',max_length = 255, unique = True)
+    state = models.BooleanField('Activo', default = True)
     created_date = models.DateField('Fecha de Creación', auto_now = False, auto_now_add = True)
     modified_date = models.DateField('Fecha de Modificación', auto_now = True, auto_now_add = False)
     active = models.ForeignKey(activeModel, on_delete=models.CASCADE, verbose_name = 'Estado Autentificación NFC')
