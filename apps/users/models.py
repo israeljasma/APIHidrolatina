63e4a59dd2973be5e2f4aa6dee03e70723a4e30e
Israel Jasma <by.darklancer@gmail.com>
2022-01-28 14:21:16 -0300

    Fix: usuarios no se podian actualizar sin tener un NFC

--- apps/users/models.py
+++ apps/users/models.py
@@ -31,7 +31,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     name = models.CharField('Nombres', max_length = 255, blank = True, null = True)
     last_name = models.CharField('Apellidos', max_length = 255, blank = True, null = True)
     # image = models.ImageField('Imagen de perfil', upload_to='perfil/', max_length=255, null=True, blank = True)
-    nfc = models.OneToOneField(identification, on_delete=models.SET_NULL, verbose_name = 'NFC', null = True)
+    nfc = models.OneToOneField(identification, on_delete=models.SET_NULL, verbose_name = 'NFC', blank = True, null = True)
     is_active = models.BooleanField(default = True)
     is_staff = models.BooleanField(default = False)
     historical = HistoricalRecords()
