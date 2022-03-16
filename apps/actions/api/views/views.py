5d9746003aab80eb85843b9500576aae69248f0b
Israel Jasma <israel.jasma@gmail.com>
2022-03-16 19:27:36 -0300

    Feat: representation list action detection

--- apps/actions/api/views/views.py
+++ apps/actions/api/views/views.py
@@ -2,9 +2,20 @@ from rest_framework import viewsets
 from rest_framework.authtoken.models import Token
 
 from apps.users.authentication_mixins import Authenication
-from apps.actions.api.serializers import actionDetectionSerializers
+from apps.actions.api.serializers import actionDetectionSerializers, actionDetectionRepresentationSerializers
 from apps.users.models import User
 
 class actionDetectionViewSet(viewsets.ModelViewSet):
-    serializer_class = actionDetectionSerializers
-    queryset = serializer_class.Meta.model.objects.all()
+    def get_serializer_class(self):
+        if self.action=='list':
+            serializer_class = actionDetectionRepresentationSerializers
+            return serializer_class
+        else:
+            serializer_class = actionDetectionSerializers
+            return serializer_class
+
+    def get_queryset(self):
+        return self.get_serializer().Meta.model.objects.all()
+
+    # serializer_class = actionDetectionSerializers
+    # queryset = serializer_class.Meta.model.objects.all()
