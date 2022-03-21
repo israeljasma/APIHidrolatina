f2c3fa6b3e62d113361c7a41271aa2f048899762
Israel Jasma <israel.jasma@gmail.com>
2022-03-21 05:29:17 -0300

    Feat: Router usernfc

--- apps/users/api/routers.py
+++ apps/users/api/routers.py
@@ -2,10 +2,11 @@ from django.db import router
 from rest_framework import urlpatterns
 from rest_framework.routers import DefaultRouter
 
-from apps.users.api.api import UserViewSet
+from apps.users.api.api import UserViewSet, UserNFCViewSet
 
 router = DefaultRouter()
 
-router.register(r'', UserViewSet, basename="users")
+router.register(r'users', UserViewSet, basename="users")
+router.register(r'usernfc', UserNFCViewSet, basename="usernfc")
 
 urlpatterns = router.urls
